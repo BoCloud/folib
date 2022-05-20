e0471d22cba500f4c2f220c42fdde7bbbd66ab08
xuxinping <xuxinpingxuxinping@beyondcent.com>
2022-05-20 19:12:16 +0800

    添加 DockerArtifactCoordinatesAdapter

---
+++ folib-storage/folib-storage-layout-providers/folib-storage-docker-layout-provider/src/main/java/com/veadan/folib/artifact/coordinates/DockerArtifactCoordinatesAdapter.java
@@ -0,0 +1,25 @@
+package com.veadan.folib.artifact.coordinates;
+
+import com.veadan.folib.artifact.coordinates.versioning.SemanticVersion;
+import com.veadan.folib.db.schema.Vertices;
+import com.veadan.folib.gremlin.adapters.LayoutArtifactCoordinatesAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author sbespalov
+ */
+@Component
+public class DockerArtifactCoordinatesAdapter extends LayoutArtifactCoordinatesAdapter<DockerArtifactCoordinates, String>
+{
+    public DockerArtifactCoordinatesAdapter()
+    {
+        super(Vertices.DOCKER_ARTIFACT_COORDINATES, DockerArtifactCoordinates.class);
+    }
+
+    @Override
+    protected DockerArtifactCoordinates newInstance()
+    {
+        return new DockerArtifactCoordinates();
+    }
+
+}
