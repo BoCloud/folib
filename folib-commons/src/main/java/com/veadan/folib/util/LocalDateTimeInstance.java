ee7cae0eff6c63f27aa9c512a62bed132161e45f
lph <leipenghui@gphmro.com>
2022-11-24 12:07:04 +0800

    1、LocalDateTimeInstance 时区改为获取系统时区

--- folib-commons/src/main/java/com/veadan/folib/util/LocalDateTimeInstance.java
+++ folib-commons/src/main/java/com/veadan/folib/util/LocalDateTimeInstance.java
@@ -7,13 +7,10 @@ import java.time.LocalDateTime;
 /**
  * @author Veadan
  */
-public class LocalDateTimeInstance
-{
+public class LocalDateTimeInstance {
 
-    public static LocalDateTime now()
-    {
-        Clock microsecondClock = Clock.tick(Clock.systemUTC(), Duration.ofNanos(1000000));
-
+    public static LocalDateTime now() {
+        Clock microsecondClock = Clock.tick(Clock.systemDefaultZone(), Duration.ofNanos(1000000));
         return LocalDateTime.now(microsecondClock);
     }
 
