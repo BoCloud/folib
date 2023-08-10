294178854c950f90b962855d6ae5225967692806
xiaodong.wang <wangxiaodong@beyondcent.com>
2023-08-10 15:23:15 +0800

    增加Cocoapods制品索引源码包路径属性
    Took 58 seconds

--- folib-storage/folib-storage-layout-providers/folib-storage-cocoapods-layout-provider/src/main/java/com/veadan/folib/artifact/coordinates/CocoapodsArtifactCoordinates.java
+++ folib-storage/folib-storage-layout-providers/folib-storage-cocoapods-layout-provider/src/main/java/com/veadan/folib/artifact/coordinates/CocoapodsArtifactCoordinates.java
@@ -15,23 +15,24 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @date 2023/7/28 17:44
  * @since x.x.x
  */
-@NodeEntity(Vertices.RPM_ARTIFACT_COORDINATES)
+@NodeEntity(Vertices.COCOAPODS_ARTIFACT_COORDINATES)
 @XmlRootElement(name = "CocoapodsArtifactCoordinates")
 @XmlAccessorType(XmlAccessType.NONE)
 @ArtifactCoordinatesLayout(name = CocoapodsArtifactCoordinates.LAYOUT_NAME, alias = CocoapodsArtifactCoordinates.LAYOUT_ALIAS)
 public class CocoapodsArtifactCoordinates extends 
         LayoutArtifactCoordinatesEntity<CocoapodsArtifactCoordinates, String> {
 
-//    public static final String LAYOUT_NAME = "CocoaPods";
     public static final String LAYOUT_NAME = "cocoapods";
 
     public static final String LAYOUT_ALIAS = LAYOUT_NAME;
     
     private static final String NAME = "name";
+    
+    private static final String PATH = "path";
 
 
     public CocoapodsArtifactCoordinates() {
-        resetCoordinates(NAME);
+        resetCoordinates(NAME, PATH);
     }
     
     public CocoapodsArtifactCoordinates(String name) {
@@ -40,6 +41,12 @@ public class CocoapodsArtifactCoordinates extends
         this.setName(name);
     }
     
+    public CocoapodsArtifactCoordinates(String name, String path) {
+        this();
+        
+        this.setName(name);
+        this.setPath(path);
+    }
 
     @Override
     public String getId() {
@@ -67,5 +74,16 @@ public class CocoapodsArtifactCoordinates extends
     public void setName(String name)
     {
         setCoordinate(NAME, name);
+    }
+
+    @ArtifactLayoutCoordinate
+    @XmlAttribute(name = PATH)
+    public String getPath()
+    {
+        return getCoordinate(PATH);
+    }
+
+    public void setPath(String path) {
+        setCoordinate(PATH, path);
     }
 }
