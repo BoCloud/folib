2dcae4f8c281f17d193977be717aa9bbf975e953
lph <leipenghui@gphmro.com>
2023-09-13 00:05:55 +0800

    docker登录https

--- folib-web-core/src/main/java/com/veadan/folib/controllers/layout/docker/DockerArtifactController.java
+++ folib-web-core/src/main/java/com/veadan/folib/controllers/layout/docker/DockerArtifactController.java
@@ -124,19 +124,7 @@ public class DockerArtifactController extends BaseArtifactController {
         response.reset();
         response.setDateHeader("Date", System.currentTimeMillis());
         response.setHeader(DockerApiHeader.DOCKER_DISTRIBUTION_API_VERSION.key(), DockerApiHeader.DOCKER_DISTRIBUTION_API_VERSION.value());
-
-        //如果是nginxhttps的，则应该获在nignx里面配置：proxy_set_header X-Forwarded-Proto $scheme;
-        String originalProtocol = request.getHeader("X-Forwarded-Proto");
-
-        if (originalProtocol != null && originalProtocol.equals("https")) {
-            // 使用HTTPS协议
-            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", originalProtocol+":"+request.getServerName()+request.getRequestURI(), request.getServerName()));
-        } else {
-            // 使用HTTP协议
-            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", request.getRequestURL(), request.getServerName() + ":" + request.getServerPort()));
-        }
-
-
+        setTokenUrl(request, response);
         if (Objects.isNull(authorization)) {
             return new ResponseEntity<>(unAuth(), HttpStatus.UNAUTHORIZED);
         }
@@ -202,7 +190,7 @@ public class DockerArtifactController extends BaseArtifactController {
     ) {
         try {
             if (Objects.isNull(authentication)) {
-                response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", request.getRequestURL().substring(0, request.getRequestURL().indexOf("/v2/") + 4), request.getServerName() + ":" + request.getServerPort()));
+                setTokenUrl(request, response);
                 return new ResponseEntity<>(unAuth(), HttpStatus.UNAUTHORIZED);
             }
             SpringSecurityUser userDetails = (SpringSecurityUser) authentication.getPrincipal();
@@ -277,7 +265,7 @@ public class DockerArtifactController extends BaseArtifactController {
 
     ) {
         if (Objects.isNull(authentication)) {
-            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", request.getRequestURL().substring(0, request.getRequestURL().indexOf("/v2/") + 4), request.getServerName() + ":" + request.getServerPort()));
+            setTokenUrl(request, response);
             return new ResponseEntity<>(unAuth(), HttpStatus.UNAUTHORIZED);
         }
         SpringSecurityUser userDetails = (SpringSecurityUser) authentication.getPrincipal();
@@ -515,7 +503,7 @@ public class DockerArtifactController extends BaseArtifactController {
                                                  @RequestBody byte[] bytes
     ) {
         if (Objects.isNull(authentication)) {
-            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", request.getRequestURL().substring(0, request.getRequestURL().indexOf("/v2/") + 4), request.getServerName() + ":" + request.getServerPort()));
+            setTokenUrl(request, response);
             return new ResponseEntity<>(unAuth(), HttpStatus.UNAUTHORIZED);
         }
         SpringSecurityUser userDetails = (SpringSecurityUser) authentication.getPrincipal();
@@ -1233,5 +1221,17 @@ public class DockerArtifactController extends BaseArtifactController {
             return false;
         }
     }
+
+    private void setTokenUrl(HttpServletRequest request, HttpServletResponse response) {
+        String originalProtocol = request.getHeader("X-Forwarded-Proto"), https = "https";
+        if (https.equals(originalProtocol)) {
+            // 使用HTTPS协议
+            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", "https://" + request.getServerName() + "/v2/", request.getServerName()));
+        } else {
+            // 使用HTTP协议
+            response.setHeader("WWW-Authenticate", MessageFormat.format("Bearer realm=\"{0}token\",service=\"{1}\"", "http://" + request.getServerName() + ":" + request.getServerPort() + "/v2/", request.getServerName() + ":" + request.getServerPort()));
+        }
+    }
+
 }
 
