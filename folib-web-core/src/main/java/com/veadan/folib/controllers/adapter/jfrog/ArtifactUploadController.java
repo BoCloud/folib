89538790b7ae7e01e80750375b68e8b65fa3cd34
xiaodong.wang <wangxiaodong@beyondcent.com>
2023-10-09 16:17:38 +0800

    完善Jfrog制品下载接口适配逻辑
    Took 32 seconds

--- folib-web-core/src/main/java/com/veadan/folib/controllers/adapter/jfrog/ArtifactUploadController.java
+++ folib-web-core/src/main/java/com/veadan/folib/controllers/adapter/jfrog/ArtifactUploadController.java
@@ -1,14 +1,12 @@
 package com.veadan.folib.controllers.adapter.jfrog;
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUnit;
+
 import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSON;
-import com.veadan.folib.dto.ArtifactUploadAdapterJfrogDto.OriginalChecksums;
-import com.veadan.folib.dto.ArtifactUploadAdapterJfrogDto.Checksums;
-
 import com.veadan.folib.components.promotion.ArtifactPromotionProviderRegistry;
 import com.veadan.folib.controllers.BaseController;
 import com.veadan.folib.dto.ArtifactUploadAdapterJfrogDto;
+import com.veadan.folib.dto.ArtifactUploadAdapterJfrogDto.Checksums;
+import com.veadan.folib.dto.ArtifactUploadAdapterJfrogDto.OriginalChecksums;
 import com.veadan.folib.promotion.ArtifactUploadTask;
 import com.veadan.folib.promotion.PromotionUtil;
 import com.veadan.folib.providers.io.RepositoryPath;
@@ -18,8 +16,7 @@ import com.veadan.folib.repository.MavenRepositoryFeatures;
 import com.veadan.folib.services.ArtifactManagementService;
 import com.veadan.folib.services.ArtifactMetadataService;
 import com.veadan.folib.services.RepositoryManagementService;
-import com.veadan.folib.storage.repository.Repository;
-import com.veadan.folib.web.RepositoryMapping;
+import com.veadan.folib.users.userdetails.SpringSecurityUser;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -27,15 +24,14 @@ import org.apache.tika.Tika;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +40,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.inject.Inject;
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.nio.file.Files;
 import java.util.Collections;
 import java.util.Date;
@@ -85,14 +80,12 @@ public class ArtifactUploadController extends BaseController
     
     @PreAuthorize("authenticated")
     @PutMapping(value = "/{storageId}/{repositoryId}/{artifactPath:.+}")
-    public ResponseEntity<?> upload(@RepositoryMapping Repository repository,
-                                    @PathVariable String storageId,
+    public ResponseEntity<?> upload(@PathVariable String storageId,
                                     @PathVariable String repositoryId,
                                     @PathVariable String artifactPath,
                                     @RequestParam(value = "uuid", required = false) String uuid,
-                                    @RequestHeader HttpHeaders httpHeaders, 
-                                    HttpServletRequest request, 
-                                    HttpServletResponse response) throws Exception
+                                    @RequestParam(value = "metaData", required = false) String metaData,
+                                    HttpServletRequest request) throws Exception
     {
 
         final ServletInputStream inputStream = request.getInputStream();
@@ -100,9 +93,10 @@ public class ArtifactUploadController extends BaseController
         final byte[] fileBytes = inputStream.readAllBytes();
         final MultipartFile file = new MockMultipartFile(fileName, fileBytes);
         final String baseUrl = StringUtils.chomp(configurationManager.getConfiguration().getBaseUrl(), "/");
-
-        final String fileDownUrl = String.format("%s%s", baseUrl, artifactPath);
-        String metaData = null;
+        final String fileDownUrl = String.format("%s/%s/%s/%s", baseUrl, storageId, repositoryId, artifactPath);
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        final SpringSecurityUser userDetails = (SpringSecurityUser) authentication.getPrincipal();
+        final String userName = Optional.ofNullable(userDetails).map(SpringSecurityUser::getUsername).orElse(null);
 
         final ArtifactUploadTask artifactUploadTask = new ArtifactUploadTask(storageId, repositoryId, file,
                 repositoryManagementService, repositoryPathResolver, artifactManagementService, promotionUtil, 
@@ -125,7 +119,7 @@ public class ArtifactUploadController extends BaseController
         artifactUploadAdapterJfrogDto.setRepo(repositoryId);
         artifactUploadAdapterJfrogDto.setPath(artifactPath);
         artifactUploadAdapterJfrogDto.setCreated(DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
-        artifactUploadAdapterJfrogDto.setCreatedBy("admin");
+        artifactUploadAdapterJfrogDto.setCreatedBy(userName);
         artifactUploadAdapterJfrogDto.setDownloadUri(fileDownUrl);
         artifactUploadAdapterJfrogDto.setMimeType(new Tika().detect(fileBytes));
         artifactUploadAdapterJfrogDto.setSize(String.valueOf(fileBytes.length));
