f4ed96b041557937ad6c5d43e018118108698606
lph <leipenghui@gphmro.com>
2023-11-01 15:11:40 +0800

    maven metadata优化

--- folib-storage/folib-storage-layout-providers/folib-storage-maven-layout/folib-maven-metadata-api/src/main/java/com/veadan/folib/storage/metadata/VersionCollector.java
+++ folib-storage/folib-storage-layout-providers/folib-storage-maven-layout/folib-maven-metadata-api/src/main/java/com/veadan/folib/storage/metadata/VersionCollector.java
@@ -212,6 +212,9 @@ public class VersionCollector
         {
             String unixBasedFilePath = FilenameUtils.separatorsToUnix(filePath.toString());
             Gav gav = M2_GAV_CALCULATOR.pathToGav(unixBasedFilePath);
+            if (Objects.isNull(gav)) {
+                continue;
+            }
             //TODO 非空判断 其他仓库的metadata.xml
             Artifact artifact = new DefaultArtifact(gav.getGroupId(),
                                                     gav.getArtifactId(),
