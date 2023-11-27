26bec6b70b16a0ce0dcdabf9344857d5f8cc416e
lph <leipenghui@gphmro.com>
2023-11-27 14:31:08 +0800

    commit

--- folib-cron/folib-cron-tasks/src/main/java/com/veadan/folib/cron/jobs/cleanup/GeneralCleanupArtifactsProvider.java
+++ folib-cron/folib-cron-tasks/src/main/java/com/veadan/folib/cron/jobs/cleanup/GeneralCleanupArtifactsProvider.java
@@ -1,11 +1,15 @@
 package com.veadan.folib.cron.jobs.cleanup;
 
+import com.google.common.collect.Lists;
+import com.veadan.folib.configuration.ConfigurationManager;
 import com.veadan.folib.domain.Artifact;
 import com.veadan.folib.providers.io.RepositoryFiles;
 import com.veadan.folib.providers.io.RepositoryPath;
 import com.veadan.folib.providers.io.RepositoryPathResolver;
 import com.veadan.folib.services.ArtifactManagementService;
+import com.veadan.folib.util.RepositoryPathUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +17,7 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -31,61 +36,95 @@ public class GeneralCleanupArtifactsProvider implements CleanupArtifactsProvider
     @Inject
     private RepositoryPathResolver repositoryPathResolver;
 
+    @Inject
+    private ConfigurationManager configurationManager;
+
+    private static final Integer ONE = 1;
+
+    private static final Integer ZERO = 0;
+
     @PostConstruct
     @Override
     public void register() {
         cleanupArtifactsProviderRegistry.addProvider("GENERAL", this);
-        log.info("Registered cleanup repository cron job '{}' with alias '{}'.",
+        log.info("Registered cleanup repository cron job '[{}]' with alias '[{}]'.",
                 getClass().getCanonicalName(), "GENERAL");
     }
 
     @Override
-    public String cleanup(String storageId, String repositoryId, String path, String cleanDay) throws Exception {
-        long tempDay = Long.parseLong(cleanDay);
+    public void cleanup(String storageId, String repositoryId, String path, String storageDay, String storageCondition) {
+        try {
+            RepositoryPath repositoryPath = repositoryPathResolver.resolve(storageId, repositoryId, path);
+            List<String> artifactPaths = RepositoryPathUtil.getFileRelativePaths(repositoryPath);
+            if (CollectionUtils.isEmpty(artifactPaths)) {
+                log.info("Repository storageId [{}] repositoryId [{}] not found artifacts", storageId, repositoryId);
+                return;
+            }
+            log.info("Start cleanup artifact job [ storageId [{}] repositoryId [{}] storageCondition [{}] storageDay [{}]  artifactPaths [{}]", storageId, repositoryId, storageCondition, storageDay, artifactPaths.size());
+            List<Integer> resultList = Lists.newArrayList();
+            for (String artifactPath : artifactPaths) {
+                try {
+                    Integer result = cleanupArtifact(storageId, repositoryId, artifactPath, storageDay);
+                    if (Objects.nonNull(result)) {
+                        resultList.add(result);
+                    }
+                } catch (Exception ex) {
+                    log.error("Cleanup artifact job storageId [{}] repositoryId [{}] storageCondition [{}] storageDay [{}]  path [{}] error [{}]", storageId, repositoryId, storageCondition, storageDay, path, ExceptionUtils.getStackTrace(ex));
+                }
+            }
+            long success = resultList.stream().filter(ONE::equals).count(), fail = resultList.stream().filter(ZERO::equals).count();
+            log.info("Cleanup artifact job storageId [{}] repositoryId [{}] storageCondition [{}] storageDay [{}]  success [{}] fail [{}]",
+                    storageId, repositoryId, storageCondition, storageDay, success, fail);
+        } catch (Exception e) {
+            log.error("Cleanup artifact job storageId [{}] repositoryId [{}] storageCondition [{}] storageDay [{}]  error [{}]", storageId, repositoryId, storageCondition, storageDay, ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    private Integer cleanupArtifact(String storageId, String repositoryId, String path, String storageDay) throws Exception {
+        long tempDay = Long.parseLong(storageDay);
         RepositoryPath repositoryPath = repositoryPathResolver.resolve(storageId, repositoryId, path);
         if (Objects.isNull(repositoryPath) || !Files.exists(repositoryPath)) {
-            log.warn("Cleanup storageId {} repositoryId {} path {} file not exists", storageId, repositoryId, path);
+            log.warn("Cleanup storageId [{}] repositoryId [{}] path [{}] file not exists", storageId, repositoryId, repositoryPath);
+            return null;
+        }
+        if (Files.isDirectory(repositoryPath)) {
+            log.warn("Cleanup storageId [{}] repositoryId [{}] path [{}] is directory skip", storageId, repositoryId, repositoryPath);
             return null;
         }
         if (RepositoryFiles.isTrash(repositoryPath)) {
-            log.info("Cleanup storageId {} repositoryId {} path {} is trash file skip", storageId, repositoryId, path);
+            log.info("Cleanup storageId [{}] repositoryId [{}] path [{}] is trash file skip", storageId, repositoryId, path);
             return null;
         }
         if (RepositoryFiles.isTemp(repositoryPath)) {
-            log.info("Cleanup storageId {} repositoryId {} path {} is temp file skip", storageId, repositoryId, path);
+            log.info("Cleanup storageId [{}] repositoryId [{}] path [{}] is temp file skip", storageId, repositoryId, path);
             return null;
         }
         if (RepositoryFiles.isChecksum(repositoryPath)) {
-            log.info("Cleanup storageId {} repositoryId {} path {} is checksum file skip", storageId, repositoryId, path);
+            log.info("Cleanup storageId [{}] repositoryId [{}] path [{}] is checksum file skip", storageId, repositoryId, path);
             return null;
         }
         Artifact artifact = repositoryPath.getArtifactEntry();
         if (null == artifact || null == artifact.getLastUsed()) {
-            log.warn("Cleanup storageId {} repositoryId {} path {} artifact not found", storageId, repositoryId, path);
+            log.warn("Cleanup storageId [{}] repositoryId [{}] path [{}] artifact not found", storageId, repositoryId, path);
             return null;
         }
         //获取仓库下制品最近使用时间做比较
-        LocalDateTime localDateTime = artifact.getLastUsed();
-        //保留N天的制品
-        log.info("Cleanup storageId {} repositoryId {} artifact {} time {} current time {}", storageId, repositoryId, artifact.getArtifactPath(), localDateTime, LocalDateTime.now());
-        if (!LocalDateTime.now().minusDays(tempDay).isBefore(localDateTime)) {
+        LocalDateTime lastUsedTime = artifact.getLastUsed();
+        log.info("Cleanup storageId [{}] repositoryId [{}] storageDay [{}] path [{}] lastUsedTime [{}] current time [{}]", storageId, repositoryId, storageDay, artifact.getArtifactPath(), lastUsedTime, LocalDateTime.now());
+        if (!LocalDateTime.now().minusSeconds(tempDay).isBefore(lastUsedTime)) {
             try {
-                log.info("Cleanup storageId {} repositoryId {} path {} do delete", storageId, repositoryId, repositoryPath.toString());
+                log.info("Cleanup storageId [{}] repositoryId [{}] path [{}] do delete", storageId, repositoryId, repositoryPath.toString());
                 artifactManagementService.delete(repositoryPath, true);
-                RepositoryPath parent = null;
-                try {
-                    parent = repositoryPath.getParent();
-                    if (Files.exists(parent) && !Files.isSameFile(repositoryPath.getRoot(), parent) && Files.list(parent).count() == 0) {
-                        Files.deleteIfExists(parent);
-                        log.info("Delete parent root path {}", parent.toString());
-                    }
-                } catch (Exception ex) {
-                    log.warn("Cleanup storageId {} repositoryId {} parent path {} error {}", storageId, repositoryId, parent, ExceptionUtils.getStackTrace(ex));
+                RepositoryPath parentRepositoryPath = null;
+                parentRepositoryPath = repositoryPath.getParent();
+                if (Files.exists(parentRepositoryPath) && !Files.isSameFile(repositoryPath.getRoot(), parentRepositoryPath) && Files.list(parentRepositoryPath).count() == 0) {
+                    Files.deleteIfExists(parentRepositoryPath);
+                    log.info("Cleanup storageId [{}] repositoryId [{}] parent path [{}] do delete", storageId, repositoryId, parentRepositoryPath.toString());
                 }
-                return "ok";
+                return ONE;
             } catch (Exception e) {
-                log.error("Cleanup storageId {} repositoryId {} path {} error {}", storageId, repositoryId, path, ExceptionUtils.getStackTrace(e));
-                return "fail";
+                log.error("Cleanup storageId [{}] repositoryId [{}] path [{}] error [{}]", storageId, repositoryId, path, ExceptionUtils.getStackTrace(e));
+                return ZERO;
             }
         }
         return null;
