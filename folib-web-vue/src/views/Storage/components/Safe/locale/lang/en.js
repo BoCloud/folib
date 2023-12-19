407dcc331923ab164c753c5d68b93b062047bb78
linhongyan <linhongyan@beyondcent.com>
2023-12-19 12:35:50 +0800

    制品仓库-制品库详情-统计页面国际化

---
+++ folib-web-vue/src/views/Storage/components/Safe/locale/lang/en.js
@@ -0,0 +1,14 @@
+export default {
+    'Products': 'Products',
+    'ProductsNum': 'Total number of products (PCS)',
+    'Download': 'Download',
+    'DownloadsNum': 'Total number of downloads (times)',
+    'Dependency': 'Dependency',
+    'DependenciesNum': 'Total number of dependencies',
+    'Bug': 'Bug',
+    'BugNum': 'Total number of vulnerabilities',
+    'WhiteList': 'The white list',
+    'bugWhitelist': 'vulnerability whitelist',
+    'Blacklist': 'Blacklist',
+    'bugBlacklist': 'Vulnerability blacklist',
+}
