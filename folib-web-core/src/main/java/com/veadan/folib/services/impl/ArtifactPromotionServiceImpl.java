2f5e8fc4b66caf4975d56b4d20628876b8269efc
lph <leipenghui@gphmro.com>
2024-02-22 17:49:29 +0800

    1、复制修复

--- folib-web-core/src/main/java/com/veadan/folib/services/impl/ArtifactPromotionServiceImpl.java
+++ folib-web-core/src/main/java/com/veadan/folib/services/impl/ArtifactPromotionServiceImpl.java
@@ -280,7 +280,7 @@ public class ArtifactPromotionServiceImpl implements ArtifactPromotionService {
                 Repository destRepository = repositoryManagementService.getStorage(targetStorageId).getRepository(targetRepositoryId);
                 Repository srcRepository = repositoryManagementService.getStorage(sourceStorageId).getRepository(sourceRepositoryId);
                 RepositoryPath srcPath = repositoryPathResolver.resolve(sourceStorageId, sourceRepositoryId, sourceArtifactPath);
-                promotionUtil.executeCopy(srcPath, destRepository, srcRepository);
+                promotionUtil.executeCopy(srcPath, srcRepository, destRepository);
                 return ResponseEntity.ok("ok");
             }
             if (Objects.isNull(syncModel)) {
