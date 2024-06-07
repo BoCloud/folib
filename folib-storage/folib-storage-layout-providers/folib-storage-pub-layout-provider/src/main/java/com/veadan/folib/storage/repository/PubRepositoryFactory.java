9e4cf33560d9723fa5096451d72b65f13dab165a
lph <leipenghui@gphmro.com>
2024-06-07 16:52:48 +0800

    commit

---
+++ folib-storage/folib-storage-layout-providers/folib-storage-pub-layout-provider/src/main/java/com/veadan/folib/storage/repository/PubRepositoryFactory.java
@@ -0,0 +1,34 @@
+package com.veadan.folib.storage.repository;
+
+import com.veadan.folib.providers.layout.PubLayoutProvider;
+import com.veadan.folib.repository.NpmRepositoryFeatures;
+
+import javax.inject.Inject;
+import java.util.LinkedHashSet;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Veadan
+ */
+@Component
+public class NpmRepositoryFactory
+        implements RepositoryFactory
+{
+
+    @Inject
+    private NpmRepositoryFeatures npmRepositoryFeatures;
+
+
+    @Override
+    public RepositoryDto createRepository(String repositoryId)
+    {
+        RepositoryDto repository = new RepositoryDto(repositoryId);
+        repository.setLayout(PubLayoutProvider.ALIAS);
+        repository.setArtifactCoordinateValidators(
+                new LinkedHashSet<>(npmRepositoryFeatures.getDefaultArtifactCoordinateValidators()));
+
+        return repository;
+    }
+
+}
