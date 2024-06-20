d55e84dc03ae475a97ea6293ac3300eebdf93b04
彭骄 <pengjiao@beyondcent.com>
2024-06-20 10:20:10 +0800

    feat: hugging face 文件预览

--- folib-web-core/src/main/java/com/veadan/folib/controllers/layout/huggingface/HuggingFaceMLControllers.java
+++ folib-web-core/src/main/java/com/veadan/folib/controllers/layout/huggingface/HuggingFaceMLControllers.java
@@ -8,14 +8,18 @@ import com.veadan.folib.domain.huggingface.model.request.MlFilesRequest;
 import com.veadan.folib.domain.huggingface.model.request.MlFilesResponse;
 import com.veadan.folib.domain.huggingface.model.request.MlModelRequestContext;
 import com.veadan.folib.domain.huggingface.repository.MlModelLocalRepository;
+import com.veadan.folib.providers.io.RepositoryPath;
 import com.veadan.folib.providers.layout.HuggingFaceLayoutProvider;
 import com.veadan.folib.storage.repository.Repository;
 import com.veadan.folib.web.LayoutRequestMapping;
 import com.veadan.folib.web.RepositoryMapping;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -422,6 +426,29 @@ public class HuggingFaceMLControllers extends BaseArtifactController {
     )
     public ResponseEntity<?> passMetadataValidation(InputStream body) {
         return ResponseEntity.ok("{}");
+    }
+
+
+    @ApiOperation(value = "Used to retrieve an artifact")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = ""),
+            @ApiResponse(code = 404, message = "Requested path not found."),
+            @ApiResponse(code = 500, message = "Server error."),
+            @ApiResponse(code = 503, message = "Repository currently not in service.")})
+    @PreAuthorize("hasAuthority('ARTIFACTS_RESOLVE')")
+    @RequestMapping(value = {"/{storageId}/{repositoryId}/{artifactPath:.+}"}, method = {RequestMethod.GET, RequestMethod.HEAD})
+    public void download(@RepositoryMapping Repository repository,
+                         @PathVariable String artifactPath,
+                         @RequestHeader HttpHeaders httpHeaders, HttpServletRequest request,
+                         HttpServletResponse response)
+            throws Exception {
+        final String storageId = repository.getStorage().getId();
+        final String repositoryId = repository.getId();
+        logger.info("Requested get docker application file {}/{}/{}.", storageId, repositoryId, artifactPath);
+        RepositoryPath repositoryPath = repositoryPathResolver.resolve(storageId, repositoryId, artifactPath);
+        vulnerabilityBlock(repositoryPath);
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("Content-Disposition", "attachment;" + repositoryPath.getFileName().toString());
+        provideArtifactDownloadResponse(request, response, httpHeaders, repositoryPath);
     }
 
 }
