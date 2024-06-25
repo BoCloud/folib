ad04c9a23bbc9a674e08155bc2db1fc569878c2d
lph <leipenghui@gphmro.com>
2024-06-25 15:45:54 +0800

    1、代理库存活验证逻辑，增加对于新增仓库、删除仓库的处理

--- folib-storage/folib-storage-api/src/main/java/com/veadan/folib/services/support/RepositoryCreatedEventListener.java
+++ folib-storage/folib-storage-api/src/main/java/com/veadan/folib/services/support/RepositoryCreatedEventListener.java
@@ -7,21 +7,21 @@ import com.veadan.folib.services.TrustStoreService;
 import com.veadan.folib.storage.repository.Repository;
 import com.veadan.folib.storage.repository.RepositoryData;
 import com.veadan.folib.storage.repository.remote.RemoteRepository;
-
-import javax.inject.Inject;
-import java.io.IOException;
-
+import com.veadan.folib.storage.repository.remote.heartbeat.RemoteRepositoriesHeartbeatMonitorInitiator;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
+import java.io.IOException;
+
 /**
  * @author veadan
  */
 @Component
-public class RepositoryCreatedEventListener
-{
+public class RepositoryCreatedEventListener {
 
     private static final Logger logger = LoggerFactory.getLogger(RepositoryCreatedEventListener.class);
 
@@ -32,37 +32,31 @@ public class RepositoryCreatedEventListener
     @Inject
     private TrustStoreService trustStoreService;
 
+    @Inject
+    private RemoteRepositoriesHeartbeatMonitorInitiator remoteRepositoriesHeartbeatMonitorInitiator;
+
     @EventListener
-    public void handle(RepositoryEvent event)
-    {
-        if (event.getType() != RepositoryEventTypeEnum.EVENT_REPOSITORY_CREATED.getType())
-        {
+    public void handle(RepositoryEvent event) {
+        if (event.getType() != RepositoryEventTypeEnum.EVENT_REPOSITORY_CREATED.getType()) {
             return;
         }
 
         Repository repository = configurationManager.getConfiguration().getStorage(event.getStorageId()).getRepository(
                 event.getRepositoryId());
 
-        if (((RepositoryData)repository).getRemoteRepository() != null)
-        {
-            initializeRemoteRepository(repository.getRemoteRepository());
+        if (((RepositoryData) repository).getRemoteRepository() != null) {
+            initializeRemoteRepository(repository, repository.getRemoteRepository());
         }
     }
 
-    private void initializeRemoteRepository(RemoteRepository remoteRepository)
-    {
-        if (remoteRepository.isAutoImportRemoteSSLCertificate())
-        {
-            try
-            {
+    private void initializeRemoteRepository(Repository repository, RemoteRepository remoteRepository) {
+        remoteRepositoriesHeartbeatMonitorInitiator.scheduleRemoteRepositoryMonitoring(remoteRepositoriesHeartbeatMonitorInitiator.getDefaultRemoteRepositoriesHeartbeatIntervalSeconds(), repository.getStorageIdAndRepositoryId());
+        if (remoteRepository.isAutoImportRemoteSSLCertificate()) {
+            try {
                 trustStoreService.addSslCertificatesToTrustStore(remoteRepository.getUrl());
-            }
-            catch (IOException | TrustStoreCertificateOperationException e)
-            {
+            } catch (IOException | TrustStoreCertificateOperationException e) {
                 logger.error("Could not import remote SSL certificate to trust store", e);
             }
         }
     }
-
-
 }
