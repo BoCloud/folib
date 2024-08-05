59fc0e44679671ecb18dc6ca4bbccd9255be3ec6
lph <leipenghui@gphmro.com>
2024-08-05 11:10:33 +0800

    commit

---
+++ folib-web-core/src/main/resources/db/sql/folib-artifact-sync-slave-record-update.sql
@@ -0,0 +1,5 @@
+alter table artifact_sync_slave_record
+    add file_size BIGINT UNSIGNED  DEFAULT 0 comment '文件大小(单位是字节)';
+
+alter table artifact_sync_slave_record
+    add file_size BIGINT UNSIGNED  DEFAULT 0 comment '文件大小(单位是字节)';
