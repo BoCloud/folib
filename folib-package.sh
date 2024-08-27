528d7f18a8bc8f7f6aead161df1fb3ad5300a93e
冯茂根 <fengmaogen@beyondcent.com>
2024-08-27 10:58:57 +0800

    部署打包

--- folib-package.sh
+++ folib-package.sh
@@ -6,3 +6,8 @@ yarn install
 yarn run build
 cd ..
 mvn  package --settings folib-settings.xml -DskipTests -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
+
+cd folib-distribution
+tar -zvf folib-distribution-1.0-SNAPSHOT.tar.gz
+cd folib-distribution-1.0-SNAPSHOT
+docker build -t folib-server:1.2.6.4 .
