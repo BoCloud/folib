6877a458d3800b9add14a2568e418b534de4efbe
彭骄 <pengjiao@beyondcent.com>
2024-08-29 14:04:51 +0800

    feat: 优化rpm的xml 索引

---
+++ folib-storage/folib-storage-layout-providers/folib-storage-rpm-layout-provider/src/main/java/com/veadan/folib/metadata/model/Entry.java
@@ -0,0 +1,18 @@
+package com.veadan.folib.metadata.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Entry implements Serializable {
+    public String name;
+    public String flags;
+    public String epoch;
+    public String version;
+    public String release;
+    public String pre;
+
+    public Entry() {
+    }
+}
---
+++ folib-storage/folib-storage-layout-providers/folib-storage-rpm-layout-provider/src/main/java/com/veadan/folib/metadata/model/File.java
@@ -0,0 +1,11 @@
+package com.veadan.folib.metadata.model;
+
+import java.io.Serializable;
+
+public class File implements Serializable {
+    public String type;
+    public String path;
+
+    public File() {
+    }
+}
