01dd9eeddde1949dbb8b456cf5adb5ef12057e97
mengtao <mengtao@beyondcent.com>
2025-03-12 11:03:35 +0800

    fix(storages): 优化仓库列表切换不同仓库后再次点击子目录请求报错问题

--- folib-web-vue/src/views/Storage/Storage-components/repository-tree.vue
+++ folib-web-vue/src/views/Storage/Storage-components/repository-tree.vue
@@ -523,6 +523,7 @@ export default {
                 }
             })
             this.folibRepository = target
+            this.repositoryType = getLayoutType(this.folibRepository)
             if (this.folibRepository.type === 'proxy') {
                 this.uploadEnabled = false
                 this.copyEnabled = false
@@ -687,6 +688,15 @@ export default {
                 this.folibRepository = treeNode.data
                 this.repositoryType = getLayoutType(this.folibRepository)
                 this.queryPermission()
+            } else {
+                let target = null
+                // 获取当前子节点的最顶层父节点（仓库节点）
+                this.treeData.forEach(ele => {
+                    if (ele.id === treeNode.data.repositoryId) {
+                        target = ele
+                    }
+                })
+                this.folibRepository = target
             }
             const { storageId, id, layout } = this.folibRepository
             const { artifactPath, name } = treeNode.data
