dedf5375248ca3fcdbe2f04adf7a9c97efae079d
LingengMa <1370120724@qq.com>
2025-04-07 10:32:14 +0800

    feat: 完成登录验证

--- folib-storage/folib-storage-layout-providers/folib-storage-conda-layout-provider/src/main/java/com/veadan/folib/artifact/coordinates/CondaArtifactCoordinates.java
+++ folib-storage/folib-storage-layout-providers/folib-storage-conda-layout-provider/src/main/java/com/veadan/folib/artifact/coordinates/CondaArtifactCoordinates.java
@@ -1,6 +1,7 @@
 package com.veadan.folib.artifact.coordinates;
 
 import com.veadan.folib.artifact.coordinates.ArtifactCoordinatesLayout;
+import com.veadan.folib.artifact.coordinates.versioning.SemanticVersion;
 import com.veadan.folib.db.schema.Vertices;
 import com.veadan.folib.domain.LayoutArtifactCoordinatesEntity;
 import lombok.extern.slf4j.Slf4j;
@@ -19,42 +20,89 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.NONE)
 @ArtifactCoordinatesLayout(name = CondaArtifactCoordinates.LAYOUT_NAME, alias = CondaArtifactCoordinates.LAYOUT_ALIAS)
 public class CondaArtifactCoordinates
-        extends LayoutArtifactCoordinatesEntity<CondaArtifactCoordinates, String> {
+        extends LayoutArtifactCoordinatesEntity<CondaArtifactCoordinates, SemanticVersion> {
 
     public static final String LAYOUT_NAME = "conda";
-
     public static final String LAYOUT_ALIAS = "conda";
 
     public static final String NAME = "name";
-
     public static final String VERSION = "version";
-
     public static final String BUILD = "build";
-
-    public static final String CHECKSUM_SHA256 = "checksumSha256";
-
-    public static final String ARTIFACT_PATH = "path";
+    public static final String BUILD_NUMBER = "build_number";
+    public static final String PLATFORM = "platform";
 
     public CondaArtifactCoordinates() {
-        super();
+        resetCoordinates(NAME, VERSION, BUILD, BUILD_NUMBER, PLATFORM);
+    }
+
+    public CondaArtifactCoordinates(String name, String version, String build, String buildNumber, String platform) {
+        this();
+        setName(name);
+        setVersion(version);
+        setBuild(build);
+        setBuildNumber(buildNumber);
+        setPlatform(platform);
+    }
+
+    // 路径解析
+    public static CondaArtifactCoordinates parse(String path) {
+        return null;
     }
 
     @Override
     public String getId() {
-        return "";
+        return getName();
     }
 
+    // getters
+    @ArtifactLayoutCoordinate
+    public String getName() {
+        return getCoordinate(NAME);
+    }
+
+    @ArtifactLayoutCoordinate
+    public String getVersion() {
+        return getCoordinate(VERSION);
+    }
+
+    @ArtifactLayoutCoordinate
+    public String getBuild() {
+        return getCoordinate(BUILD);
+    }
+
+    @ArtifactLayoutCoordinate
+    public String getBuildNumber() {
+        return getCoordinate(BUILD_NUMBER);
+    }
+
+    @ArtifactLayoutCoordinate
+    public String getPlatform() {
+        return getCoordinate(PLATFORM);
+    }
+
+
+    // setters
+    public void setName(String name) {
+        setCoordinate(NAME, name);
+    }
+    private void setBuild(String build) {
+        setCoordinate(BUILD, build);
+    }
+    private void setBuildNumber(String buildNumber) {
+        setCoordinate(BUILD_NUMBER, buildNumber);
+    }
+    private void setPlatform(String platform) {
+        setCoordinate(PLATFORM, platform);
+    }
+
+
     @Override
-    public String getNativeVersion() {
-        return "";
+    public SemanticVersion getNativeVersion() {
+        return null;
     }
 
     @Override
     public String convertToPath(CondaArtifactCoordinates artifactCoordinates) {
         return "";
-    }
-
-    public static CondaArtifactCoordinates parse(String path) {
-        return new CondaArtifactCoordinates();
     }
 }
--- folib-web-core/src/main/java/com/veadan/folib/controllers/layout/conda/CondaArtifactController.java
+++ folib-web-core/src/main/java/com/veadan/folib/controllers/layout/conda/CondaArtifactController.java
@@ -1,40 +1,78 @@
 package com.veadan.folib.controllers.layout.conda;
 
+import cn.hutool.http.server.HttpServerResponse;
+import com.alibaba.fastjson.JSONObject;
 import com.veadan.folib.artifact.coordinates.CondaArtifactCoordinates;
+import com.veadan.folib.controllers.BaseArtifactController;
+import com.veadan.folib.controllers.login.LoginController;
+import com.veadan.folib.services.impl.BackupServiceImpl;
+import com.veadan.folib.storage.repository.Repository;
+import com.veadan.folib.users.userdetails.SpringSecurityUser;
+import com.veadan.folib.utils.UserUtils;
 import com.veadan.folib.web.LayoutRequestMapping;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.DELETE;
 
 /**
- * @author pengYongQiang
- * @date 1/3/2024 15:31
+ * @author LingengMa
+ * @date 2025/04/07 09:12
  */
 @RestController
 @LayoutRequestMapping(CondaArtifactCoordinates.LAYOUT_NAME)
 @Slf4j
 @Api(description = "conda坐标控制器",tags = "conda坐标控制器")
-public class CondaArtifactController {
+public class CondaArtifactController extends BaseArtifactController {
+
+    @Autowired
+    private LoginController loginController;
 
     @ApiOperation(value = "Used to authenticate an artifact")
-    @ApiResponses(value = {@ApiResponse(code = 200, message = "{token, expires_at}")})
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "{token}")})
     @PostMapping(path = "/{storageId}/{repositoryId}/authentications")
-    public ResponseEntity<Object> authenticate() {
-        return null;
+    public ResponseEntity authenticate(Authentication authentication) {
+        return loginController.formLogin(authentication);
     }
 
+    @ApiOperation(value = "Used to get the user information")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "{login, user_type}"),
+                           @ApiResponse(code = 401, message = "Unauthorized"),
+                           @ApiResponse(code = 500, message = "Internal Server Error")})
+    @PreAuthorize("hasAuthority('ARTIFACTS_VIEW')")
     @GetMapping(path = "/{storageId}/{repositoryId}/user")
-    public ResponseEntity<Object> getUser() {
-        return null;
+    public ResponseEntity<Object> getUser(HttpServletRequest request,
+                                          HttpServerResponse response,
+                                          Authentication authentication) {
+        try {
+            SpringSecurityUser currentUser = UserUtils.getSpringSecurityUser();
+            if (currentUser == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+            }
+            JSONObject responseJson = new JSONObject();
+            responseJson.put("login", currentUser.getUsername());
+            responseJson.put("user_type", currentUser.getUserType());
+            return ResponseEntity.ok(responseJson);
+
+
+        } catch (Exception e) {
+            log.error("Error getting user information", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
     @GetMapping(path = "/{storageId}/{repositoryId}/dist/{channelId}/{packageId}/{version}/{platformId}/{filename}")
