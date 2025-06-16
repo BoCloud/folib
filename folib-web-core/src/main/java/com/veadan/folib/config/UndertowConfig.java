9f0dabd1f614e7f5ef431a50ad852fb8798de9d5
彭骄 <pengjiao@beyondcent.com>
2025-06-16 15:25:58 +0800

    fix: 处理UndertowConfig

--- folib-web-core/src/main/java/com/veadan/folib/config/UndertowConfig.java
+++ folib-web-core/src/main/java/com/veadan/folib/config/UndertowConfig.java
@@ -1,8 +1,6 @@
 package com.veadan.folib.config;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.undertow.UndertowMetrics;
-import io.undertow.server.handlers.MetricsHandler;
+import io.undertow.UndertowOptions;
 import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -14,19 +12,11 @@ import org.springframework.context.annotation.Configuration;
 public class UndertowConfig {
 
     @Bean
-    public WebServerFactoryCustomizer<UndertowServletWebServerFactory> undertowCustomizer(MeterRegistry meterRegistry) {
-        return factory -> {
-            factory.addBuilderCustomizers(builder -> {
-                builder.setServerOption(io.undertow.UndertowOptions.ALLOW_ENCODED_SLASH, true);
-                builder.setServerOption(io.undertow.UndertowOptions.DECODE_URL, true);
-            });
+    public WebServerFactoryCustomizer<UndertowServletWebServerFactory> undertowCustomizer() {
+        return factory -> factory.addBuilderCustomizers(builder -> {
+            builder.setServerOption(io.undertow.UndertowOptions.ALLOW_ENCODED_SLASH, true);
+            builder.setServerOption(io.undertow.UndertowOptions.DECODE_URL, true);
 
-            factory.addDeploymentInfoCustomizers(deploymentInfo -> {
-                // Micrometer MetricsHandler 注册
-                deploymentInfo.addInitialHandlerChainWrapper(handler ->
-                        new MetricsHandler(handler, new UndertowMetrics(meterRegistry))
-                );
-            });
-        };
+        });
     }
 }
