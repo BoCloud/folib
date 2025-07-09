afc4bbcca33c0524bc713c15332f3933dc6d855d
彭骄 <pengjiao@beyondcent.com>
2025-07-09 21:41:41 +0800

    fix:代码添加版权

--- folib-web-core/src/main/java/com/folib/components/FolibApplicationRunner.java
+++ folib-web-core/src/main/java/com/folib/components/FolibApplicationRunner.java
@@ -1,3 +1,21 @@
+/*
+ * Folib - [新一代AI制品仓库]
+ * Copyright (C) 2025 bocloud.com.cn <folib@beyondcent.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 本程序是自由软件：您可依据GNU通用公共许可证（GPL-3.0+）条款重新发布和修改，
+ * 但禁止任何形式的商业售卖行为（包括但不限于：直接销售、捆绑销售、云服务商用）。
+ *
+ * This program is distributed WITHOUT ANY WARRANTY.
+ * Commercial sale of this software is expressly prohibited.
+ *
+ * For license details, see: https://www.gnu.org/licenses/gpl-3.0.html
+ * 商业授权咨询请联系：folib@beyondcent.com
+ */
 package com.folib.components;
 
 import com.folib.cluster.ClusterProperties;
@@ -19,6 +37,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.web.context.WebServerInitializedEvent;
@@ -56,7 +75,7 @@ public class FolibApplicationRunner implements ApplicationRunner, ApplicationLis
     @Autowired
     private ClusterProperties clusterProperties;
 
-    //private Server jettyServer;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -86,7 +105,8 @@ public class FolibApplicationRunner implements ApplicationRunner, ApplicationLis
         String gcGraceSeconds = System.getProperty(CassandraComponent.GC_GRACE_SECONDS_KEY, CassandraComponent.DEFAULT_GC_GRACE_SECONDS.toString());
         nodeService.modifyGcGraceSeconds(Integer.parseInt(gcGraceSeconds));
         handlerUnExecutedTask();
-        if (scanService.countProperties() <= 1) {
+        boolean enableAnalysis = Boolean.parseBoolean(System.getenv("folib.enableAnalysis"));
+        if (scanService.countProperties() <= 1 && enableAnalysis) {
             //初始化漏洞库，耗时长，放到最后
             log.info("The initialization of vulnerability data begins ");
             scanService.updateMirror();
