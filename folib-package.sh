cec8564fc7be69040cf0be478ba7c91a92a6561a
xuxinping <xuxinping@beyondcent.com>
2025-07-09 23:53:13 +0800

    修改了获取版本号参数

--- folib-package.sh
+++ folib-package.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #set -ex
+
 mvn clean --settings folib-settings.xml -Dmaven.test.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 cd folib-web-vue
+
 yarn install
 yarn run build
 cd ..
